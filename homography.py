13c8f8cd7a13cbe462cdf748b0a918f002cb4ca4
Nicholas H <syncopika@users.noreply.github.com>
2022-02-04 21:33:41 -0500

    More progress (#1)
    * more stuff
    
    * more progress?
    
    * more progress
    
    * corner picking in any order works and also the corrected image is saveable (with grayscale as well)
    
    * fix readme images
    
    * readme edit

--- homography.py
+++ homography.py
@@ -1,83 +1,63 @@
-# following: https://blog.ekbana.com/skew-correction-using-corner-detectors-and-homography-fda345e42e65
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plot
+from matplotlib.patches import Circle
+from tutorial import get_destination_points, unwarp
 
 # test.png has more rounded top corners while test2.png has sharper corners
-image = cv2.imread("test_images/test2.png")
+image = cv2.imread("test_images/building.png")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-def get_destination_points(corners):
-    """
-        Approximate height and width of rectangle if it was unwarped
 
-        args:
-            corners = list
+SELECTED_CORNERS = []
+
+class CornerSelectImage:
+    def __init__(self, image):
+        self.fig, self.ax = plot.subplots()
+        self.selected_corners = []
+        self.ax.imshow(image)
+        #height = image.shape[0]
+        #width = image.shape[1]
+        #self.ax.set_ylim([height, 0])
+        #self.ax.set_xlim([0, width])
+        self.ax.set_title("select corners")
+        self.fig.canvas.mpl_connect('button_press_event', self._click)
+        plot.show()
         
-        return:
-            list (the destination corners), int (height), int (width)
-    """
-    top_left = corners[0]
-    top_right = corners[1]
-    bottom_left = corners[2]
-    bottom_right = corners[3]
-    
-    # distance formula for calculating widths, heights
-    width1 = int(np.sqrt((top_right[0] - top_left[0])**2 + (top_right[1] - top_left[1])))
-    width2 = int(np.sqrt((bottom_right[0] - bottom_left[0])**2 + (bottom_right[1] - bottom_left[1])**2))
-    width = max(width1, width2)
-    
-    height1 = int(np.sqrt((top_right[0] - bottom_right[0])**2 + (top_right[1] - bottom_right[1])))
-    height2 = int(np.sqrt((top_left[0] - bottom_left[0])**2 + (top_left[1] - bottom_left[1])))
-    height = max(height1, height2)
-    
-    destination_corners = np.float32([(0,0), (width-1, 0), (0, height-1), (width-1, height-1)])
-    
-    print("\nThe destination points are:\n")
-    for idx, corner in enumerate(destination_corners):
-        char = chr(65 + idx) + "'"
-        print(f"{char}:{corner}")
+    def _click(self, event):
+        self.selected_corners.append((event.xdata, event.ydata))
+        self.ax.add_patch(Circle((event.xdata, event.ydata), radius=10, color='red'))
+        self.fig.canvas.draw()
+        
+        # once 4 corners selected, close image
+        if len(self.selected_corners) == 4:
+            plot.close()
 
-    print(f"\nThe approximated height and width of the original image is: \n({height},{width})")
 
-    return destination_corners, height, width
+def collect_corners(image):
+    corner_picker = CornerSelectImage(image)
+    corners = corner_picker.selected_corners
+    # we want to allow the user to select the corners in any order they want
+    # but we need the corners to be in a particular order when we use them!
+    # top left, top right, bottom left, bottom right
     
+    # sort by y coord first
+    corners.sort(key=lambda x: x[1])
     
-def get_corners(image):
-    """
-        Using Shi-Tomasi algorithm for corner detection
-    
-        args:
-            image = np.array
-            
-        return:
-            list
-    """
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    corners = cv2.goodFeaturesToTrack(gray, 4, 0.01, 100)
-    corners = np.int0(corners)
-    corners = sorted(np.concatenate(corners).tolist())
-    print("\nThe corner points are...\n")
-    
-    img = image.copy()
-    for idx, corner in enumerate(corners):
-        x, y = corner
-        cv2.circle(img, (x, y), 3, 255, -1)
-        char = chr(65 + idx)
-        print(f"{char}:{corner}")
-        cv2.putText(img, char, tuple(corner), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
-     
-    plot.imshow(img)
-    plot.title("corner detection: Shi-Tomasi")
-    plot.show()
-    
-    get_destination_points(corners)
+    # then sort by x coord, but split so that we sort the top coords separately from the bottom coords
+    corners = sorted(corners[:2], key=lambda x: x[0]) + sorted(corners[2:], key=lambda x: x[0])
     
     return corners
+    
+    
+def skew_correction(image):
+    # the general idea is: get the corners of the original, warped image.
+    # then figure out what the corners should be if the image was unwarped based on the warped image dimensions.
+    # then create a matrix to transform the warped image to the unwarped one and apply it.
+    # choosing the corners yourself is better than trying to use a contour I think
+    corners = collect_corners(image)
+    destination_corners, height, width = get_destination_points(corners)
+    unwarp(image, np.float32(corners), destination_corners)
 
-#plot.imshow(image)
-#plot.title("test image")
-#plot.show()
 
-get_corners(image)
+skew_correction(image)
+
--- readme.md
+++ readme.md
@@ -1,2 +1,29 @@
 # homography    
-learning about homography and fixing skewed images with OpenCV    
+learning about homography and fixing skewed images with OpenCV and matplotlib    
+    
+## motivation:    
+I was looking at some buildings in London via Google Maps for some inspiration for buildings to model in Blender and while looking at them from the perspective of the Google car camera thought it would awesome if I could take a snapshot of the buildings and somehow create some automation to construct at least the face of them in 3D.    
+    
+The first step in doing so I thought would be to correct the perspective of the building images taken from Google Maps. With this code, the perspective correction isn't perfect but it does seem to work as it should (see example below).    
+    
+![original, skewed image](test_images/building.png) ![corrected image](unwarped_image.png)    
+    
+This code allows a user to select 4 points that enclose the area of an image they wish to perform perspective correction on.    
+    
+## how to run:    
+I recommend using miniconda. With miniconda installed you can run the following (assuming in the directory of this repo):    
+```
+> conda create --name homography opencv numpy matplotlib    
+> conda activate homography    
+> python homography.py    
+```   
+   
+## example usage:    
+![example usage gif](example_usage.gif)    
+    
+## next steps:    
+Now we can get a mostly perspective-corrected image. With this image, I guess the next step would be to figure out how to create a 3D representation of it in Blender (just the face of the building of course). Is that particularly useful though? I dunno, maybe? But it would be neat I think. :)    
+    
+## acknowledgements:    
+many thanks to Ashuta's great tutorial on homography: https://blog.ekbana.com/skew-correction-using-corner-detectors-and-homography-fda345e42e65    
+    
