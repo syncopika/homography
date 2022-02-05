4a712a365c89d1d4c0a6fe5b08e0430a7353070d
nicholas hung <nhung93@outlook.com>
2022-02-04 21:24:06 -0500

    corner picking in any order works and also the corrected image is saveable (with grayscale as well)

--- homography.py
+++ homography.py
@@ -1,12 +1,11 @@
-# following: https://blog.ekbana.com/skew-correction-using-corner-detectors-and-homography-fda345e42e65
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plot
 from matplotlib.patches import Circle
+from tutorial import get_destination_points, unwarp
 
 # test.png has more rounded top corners while test2.png has sharper corners
-image = cv2.imread("test_images/buildingedit.png")
+image = cv2.imread("test_images/building.png")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 SELECTED_CORNERS = []
@@ -36,202 +35,24 @@ class CornerSelectImage:
 
 def collect_corners(image):
     corner_picker = CornerSelectImage(image)
+    corners = corner_picker.selected_corners
+    # we want to allow the user to select the corners in any order they want
+    # but we need the corners to be in a particular order when we use them!
+    # top left, top right, bottom left, bottom right
     
-    # TODO: reorganize points such that they come in
-    # the special order noted in the tutorial (see below)
+    # sort by y coord first
+    corners.sort(key=lambda x: x[1])
     
-    return corner_picker.selected_corners
-    
-def get_destination_points(corners):
-    """
-        Approximate height and width of rectangle if it was unwarped
-
-        args:
-            corners = list
-        
-        return:
-            list (the destination corners), int (height), int (width)
-    """
-    
-    top_left = corners[0]
-    top_right = corners[1]
-    bottom_left = corners[2]
-    bottom_right = corners[3]
-    
-    # distance formula for calculating widths, heights
-    width1 = int(np.sqrt((top_right[0] - top_left[0])**2 + (top_right[1] - top_left[1])**2))
-    width2 = int(np.sqrt((bottom_right[0] - bottom_left[0])**2 + (bottom_right[1] - bottom_left[1])**2))
-    width = max(width1, width2)
-    
-    height1 = int(np.sqrt((top_right[0] - bottom_right[0])**2 + (top_right[1] - bottom_right[1])**2))
-    height2 = int(np.sqrt((top_left[0] - bottom_left[0])**2 + (top_left[1] - bottom_left[1])**2))
-    height = max(height1, height2)
-    
-    destination_corners = np.float32([(0,0), (width-1, 0), (0, height-1), (width-1, height-1)])
-    
-    print("\nThe destination points are:\n")
-    for idx, corner in enumerate(destination_corners):
-        char = chr(65 + idx) + "'"
-        print(f"{char}:{corner}")
-
-    print(f"\nThe approximated height and width of the original image is: \n({height},{width})")
-
-    return destination_corners, height, width
-    
-def unwarp(img, src, dst):
-    """
-        Unwarp an image
-        
-        args:
-            img: np.array
-            src: list
-            dst: list
-            
-        return:
-            np.array
-    """
-    height = img.shape[0]
-    width = img.shape[1]
-    
-    matrix, _ = cv2.findHomography(src, dst, method=cv2.RANSAC, ransacReprojThreshold=3.0)
-    print(f"\nThe homography matrix is: {matrix}\n")
-    unwarped = cv2.warpPerspective(img, matrix, (width, height), flags=cv2.INTER_LINEAR)
-    
-    _, (ax1, ax2) = plot.subplots(1, 2)
-    ax1.imshow(img)
-    x = [src[0][0], src[2][0], src[3][0], src[1][0], src[0][0]]
-    y = [src[0][1], src[2][1], src[3][1], src[1][1], src[0][1]]
-    ax1.plot(x, y, color="yellow", linewidth=3)
-    ax1.set_ylim([height, 0])
-    ax1.set_xlim([0, width])
-    ax1.set_title("targeted area in original image")
-    ax2.imshow(unwarped)
-    ax2.set_title("unwarped image")
-    plot.show()
-    
-    return unwarped
-    
-def get_corners(image):
-    """
-        Using Shi-Tomasi algorithm for corner detection
-    
-        args:
-            image = np.array
-            
-        return:
-            list
-    """
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    corners = cv2.goodFeaturesToTrack(gray, 4, 0.01, 10)
-    corners = np.int0(corners)
-    corners = sorted(np.concatenate(corners).tolist())
-    print("\nThe corner points are...\n")
-    
-    img = image.copy()
-    for idx, corner in enumerate(corners):
-        x, y = corner
-        cv2.circle(img, (x, y), 3, 255, -1)
-        char = chr(65 + idx)
-        print(f"{char}:{corner}")
-        cv2.putText(img, char, tuple(corner), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
-     
-    plot.imshow(img)
-    plot.title("corner detection: Shi-Tomasi")
-    plot.show()
-    
-    dst_corners, h, w = get_destination_points(corners)
-    
-    unwarp(image, np.float32(corners), dst_corners)
+    # then sort by x coord, but split so that we sort the top coords separately from the bottom coords
+    corners = sorted(corners[:2], key=lambda x: x[0]) + sorted(corners[2:], key=lambda x: x[0])
     
     return corners
-
-def apply_filter(image):
-    """
-        Create a 5 x 5 kernel and apply the filter to a grayscaled image
-        The filter will blur the image
-        
-        args:
-            image: np.array
-            
-        return:
-            np.array
-    """
-    grayscale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    kernel = np.ones((5, 5), np.float32) / 15
-    filtered = cv2.filter2D(grayscale, -1, kernel)
-    plot.imshow(cv2.cvtColor(filtered, cv2.COLOR_BGR2RGB))
-    plot.title("filtered image")
-    plot.show()
-    
-    return filtered
-
-def apply_threshold(filtered_img):
-    """
-        Apply Otsu threshold
-        
-        args:
-            filtered_img: np.array
-            
-        return:
-            np.array
-    """
-    ret, thresh = cv2.threshold(filtered_img, 250, 255, cv2.THRESH_OTSU)
-    plot.imshow(cv2.cvtColor(thresh, cv2.COLOR_BGR2RGB))
-    plot.title("after applying Otsu threshold")
-    plot.show()
-    return thresh
-    
-def detect_contour(image, image_shape):
-    """
-        args:
-            image: np.array
-            image_shape: tuple
-            
-        return:
-            np.array, list
-    """
-    canvas = np.zeros(image_shape, np.uint8)
-    contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    cnt = sorted(contours, key=cv2.contourArea, reverse=True)[0]
-    cv2.drawContours(canvas, cnt, -1, (0, 255, 255), 3)
-    plot.title("largest contour")
-    plot.imshow(canvas)
-    plot.show()
-    
-    return canvas, cnt
-    
-def detect_corners_from_contour(canvas, contour, image):
-    """
-        detecting corner points from contours using cv2.approxPolyDP()
-        
-        args:
-            canvas: np.array
-            contour: list
-            
-        return:
-            list
-    """
-    epsilon = 0.02 * cv2.arcLength(contour, True)
-    approx_corners = cv2.approxPolyDP(contour, epsilon, True)
-    cv2.drawContours(canvas, approx_corners, -1, (255, 255, 0), 10)
-    approx_corners = sorted(np.concatenate(approx_corners).tolist())
-    print("\nThe corner points are ...\n")
-    for index, corner in enumerate(approx_corners):
-        char = chr(65 + index)
-        print(f"{char}:{corner}")
-        cv2.putText(canvas, char, tuple(corner), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-    
-    # notice the order of the corner points is important
-    approx_corners = [approx_corners[i] for i in [0, 2, 1, 3]]
-    
-    plot.imshow(canvas)
-    plot.title("corner points")
-    plot.show()
-    
-    return approx_corners
     
     
 def skew_correction(image):
+    # the general idea is: get the corners of the original, warped image.
+    # then figure out what the corners should be if the image was unwarped based on the warped image dimensions.
+    # then create a matrix to transform the warped image to the unwarped one and apply it.
     # choosing the corners yourself is better than trying to use a contour I think
     corners = collect_corners(image)
     destination_corners, height, width = get_destination_points(corners)
--- readme.md
+++ readme.md
@@ -1,2 +1,26 @@
 # homography    
-learning about homography and fixing skewed images with OpenCV    
+learning about homography and fixing skewed images with OpenCV and matplotlib    
+    
+## motivation:    
+I was looking at some buildings in London via Google Maps for some inspiration for buildings to model in Blender and while looking at them from the perspective of the Google car camera thought it would awesome if I could take a snapshot of the buildings and somehow create some automation to construct at least the face of them in 3D.    
+    
+The first step in doing so I thought would be to correct the perspective of the building images taken from Google Maps. With this code, the perspective correction isn't perfect but it does seem to work as it should (see example below).    
+    
+!(test_images/building.png)[original, skewed image] !(unwarped_image.png)[corrected image]    
+    
+This code allows a user to select 4 points that enclose the area of an image they wish to perform perspective correction on.    
+    
+## how to run:    
+I recommend using miniconda. With miniconda installed you can run the following (assuming in the directory of this repo):    
+```
+> conda create --name homography opencv numpy matplotlib    
+> conda activate homography    
+> python homography.py    
+```   
+   
+## example usage:    
+!(example_usage.gif)[example usage gif]    
+    
+## next steps:    
+Now we can get a mostly perspective-corrected image. With this image, I guess the next step would be to figure out how to create a 3D representation of it in Blender (just the face of the building of course). Is that particularly useful though? I dunno, maybe? But it would be neat I think. :)    
+    
